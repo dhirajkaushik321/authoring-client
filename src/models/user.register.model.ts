43010db4853188c87c695888476575f39847d7ae
Rupesh <rgbhoir@gmail.com>
2023-08-16 23:15:09 +0530

    merge conflict resolved packagelock

--- src/models/user.register.model.ts
+++ src/models/user.register.model.ts
@@ -3,7 +3,6 @@ type RegisterUser = {
     email: string;
     password: string;
     [key: string]: string; // Index signature
-
 };
 
 export default RegisterUser;
--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -1,7 +1,6 @@
 import Header from "../../components/Header";
 import Login from "../../components/Login";
 
-
 export default function LoginPage(){
     return(
         <>
